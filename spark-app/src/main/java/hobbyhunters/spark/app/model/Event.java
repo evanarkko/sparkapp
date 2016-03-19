e0093cf9f0f6f6468825b5b865d8b89fda35239c
Mikko Rinta-Homi <Mikkojrh@Gmail.com>
2016-03-19 07:51:40 +0200

    Added some user-scoping related classes

--- spark-app/src/main/java/hobbyhunters/spark/app/model/Event.java
+++ spark-app/src/main/java/hobbyhunters/spark/app/model/Event.java
@@ -7,7 +7,7 @@ public class Event {
     
     public Event(double price, String name) {
         this.price = price;
-        this.name = name;      
+        this.name = name;
     }
     
     public double getPrice() {

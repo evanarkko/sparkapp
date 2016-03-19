8689ba16020f8946e59496b1caf916ef8590ff10
Evan Miller <eamiller@dhcp-asv-100.helsinki.fi>
2016-03-19 02:39:45 +0200

    user

--- spark-app/src/main/java/hobbyhunters/spark/app/SparkMain.java
+++ spark-app/src/main/java/hobbyhunters/spark/app/SparkMain.java
@@ -1,12 +1,18 @@
 package hobbyhunters.spark.app;
 
+import java.util.HashMap;
+import spark.ModelAndView;
 import static spark.Spark.*;
 
 public class SparkMain {
     
     public static void main(String[] args) {
         port(Constants.PORT);
-        get("/EventSpark", (req, res) -> "Hello! We're live.");
+        get("/EventSpark", (req, res) -> {
+            HashMap map = new HashMap<>();
+
+            return new ModelAndView(map, "mainpage");
+        });
     }
 
 }
---
+++ spark-app/src/main/java/hobbyhunters/spark/app/user/User.java
@@ -0,0 +1,59 @@
+package hobbyhunters.spark.app.user;
+
+import hobbyhunters.spark.app.user.meta.PriceRange;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author eamiller
+ */
+public class User {
+    private String name;
+    private int age;
+    private PriceRange priceRange;
+    private String location;
+    private List<User> friends;
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+        this.friends = new ArrayList<>();
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public PriceRange getPriceRange() {
+        return priceRange;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPriceRange(PriceRange priceRange) {
+        this.priceRange = priceRange;
+    }
+    
+    public void addFriend(User user){
+        this.friends.add(user);
+    }
+    
+    
+    
+    
+}

110a4c206f2eb2314aa93eccaa33d4f225d16ff4
axel <agile@null.net>
2016-03-19 02:40:51 +0200

    event filter

--- spark-app/src/main/java/hobbyhunters/spark/app/model/Event.java
+++ spark-app/src/main/java/hobbyhunters/spark/app/model/Event.java
@@ -3,5 +3,18 @@ package hobbyhunters.spark.app.model;
 public class Event {
    
     private double price;
+    private String name;
     
+    public Event(double price, String name) {
+        this.price = price;
+        this.name = name;      
+    }
+    
+    public double getPrice() {
+        return price;
+    }
+    
+    public String getName() {
+        return name;
+    }
 }
